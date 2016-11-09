60d8bbf1eaf254e57e05f535a1a86e2cc1fa7bc3
lyovabo <minasyanlevon@gmai.com>
2016-11-09 19:35:19 +0400

    some fixes and fixed video

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,63 @@
 'use strict';
 var gulp = require('gulp'),
-    autoprefixer = require('autoprefixer'),
-    dedupe = require('postcss-discard-duplicates'),
-    postcss = require('gulp-postcss'),
-    csscomb = require('gulp-csscomb'),
-    scss = require('postcss-scss'),
-    browserSync = require('browser-sync').create(),
-    sass = require('gulp-sass'),
-    concat = require('gulp-concat'),  
-    rename = require('gulp-rename'),  
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    cache = require('gulp-cache');
+  autoprefixer = require('autoprefixer'),
+  dedupe = require('postcss-discard-duplicates'),
+  postcss = require('gulp-postcss'),
+  csscomb = require('gulp-csscomb'),
+  scss = require('postcss-scss'),
+  browserSync = require('browser-sync').create(),
+  sass = require('gulp-sass'),
+  concat = require('gulp-concat'),
+  rename = require('gulp-rename'),
+  uglify = require('gulp-uglify'),
+  imagemin = require('gulp-imagemin'),
+  cache = require('gulp-cache');
 
-gulp.task('watch', ['sass','browserSync'], function (){
-  gulp.watch('sass/**/*.scss', ['sass']); 
-  gulp.watch('sass/*.scss', ['sass']); 
+gulp.task('watch', ['sass', 'browserSync'], function() {
+  gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch('sass/*.scss', ['sass']);
   gulp.watch('sass/**/*.scss', browserSync.reload);
   gulp.watch('index.html', browserSync.reload);
 
 
 })
 
-gulp.task('prettycss', function () {
-    return gulp.src(['sass/**/*.css', 'sass/**/*.scss', '!sass/bootstrap/**/*.scss'], {base: './'})
-        .pipe(postcss([
-            autoprefixer({browsers: ['last 4 versions']}),
-            dedupe
-        ], {syntax: scss}))
-        .pipe(csscomb())
-        .pipe(gulp.dest('./'));
+gulp.task('prettycss', function() {
+  return gulp.src(['sass/**/*.css', 'sass/**/*.scss', '!sass/bootstrap/**/*.scss'], { base: './' })
+    .pipe(postcss([
+      autoprefixer({ browsers: ['last 4 versions'] }),
+      dedupe
+    ], { syntax: scss }))
+    .pipe(csscomb())
+    .pipe(gulp.dest('./'));
 });
-gulp.task('scripts', function() {  
-    return gulp.src(['assets/js/jquery-3.1.1.min.js','assets/js/bootstrap.min.js','assets/js/index.js'])
-        .pipe(concat('scripts.js'))
-        .pipe(gulp.dest('app/js'))
-        .pipe(rename('scripts.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('app/js'));
+gulp.task('scripts', function() {
+  return gulp.src(['assets/js/jquery-3.1.1.min.js', 'assets/js/bootstrap.min.js', 'assets/js/index.js'])
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('app/js'))
+    .pipe(rename('scripts.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'));
 });
 gulp.task('sass', function() {
-  return gulp.src(['sass/**/*.scss','sass/*.scss'])
+  return gulp.src(['sass/**/*.scss', 'sass/*.scss'])
     .pipe(sass())
     .pipe(gulp.dest('assets/css'))
 });
 
 gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
 });
 gulp.task('images', function() {
-    return gulp.src('assets/img/*.+(png|jpg|jpeg|gif|svg)')
-        .pipe(cache(imagemin({
-            interlaced: true
-        })))
-        .pipe(gulp.dest('app/images'));
+  return gulp.src('assets/img/*.+(png|jpg|jpeg|gif|svg)')
+    .pipe(cache(imagemin({
+      interlaced: true
+    })))
+    .pipe(gulp.dest('app/images'));
 });
 
 // const gulp = require('gulp'),
